a0a9e1f5e4f88f34f32070e29c32edaee946e374
NasibNia <nasibnia@gmail.com>
2018-08-13 21:27:53 -0700

    added a bit of styling

--- javascript/gifScript.js
+++ javascript/gifScript.js
@@ -3,7 +3,7 @@ var array  = ["happy", "angry", "excited", "grateful", "scared" , "chilled", "co
 var limit  = 10;
 var offset = 1;
 var previousSrch = "";
-
+var trackerObj = {};
 
 for (var i = 0 ; i < array.length ; i++){
     createButton (array[i]);
@@ -11,7 +11,7 @@ for (var i = 0 ; i < array.length ; i++){
 
 function createButton (str){
     var newButton = $('<button>');
-    newButton.addClass("BtnClass");
+    newButton.addClass("BtnClass   light bg-primary border-9 border-success border rounded-circle mr-3 p-3 shadow-lg");
     newButton.text(str);
     newButton.attr("value", str);
     $("#button-area").append(newButton);
@@ -24,8 +24,9 @@ $(document).on('click', '.BtnClass', function (){
     if (srchInput !== previousSrch) {
         $("#display-area").empty();
     }
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + srchInput + "&api_key=dc6zaTOxFJmzC&limit=" + limit+"&offset=" + offset + "&tag=emotions";
+    // "&api_key=dc6zaTOxFJmzC&limit="
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + srchInput +"&api_key=bcTN4v12kgrx5zKDIHlp2YxG77wxGLiB&limit="
+    + limit+"&offset=" + offset + "&tag=emotions";
     offset += limit;
 
     $.ajax({
@@ -34,6 +35,9 @@ $(document).on('click', '.BtnClass', function (){
     }).then(function(response) {
         console.log(response);
         for (var i = 0 ; i < limit ; i++){
+            
+            var gifBlock = $('<div class="gifBox">');
+            var floatDiv = $("<div class='img-container'>");
             var showImage = $('<img>');
             console.log(response.data[i].images);
             console.log(response.data[i].images.fixed_height_still.url);
@@ -41,9 +45,18 @@ $(document).on('click', '.BtnClass', function (){
             showImage.attr("data-still",response.data[i].images.fixed_height_still.url);
             showImage.attr("data-animate",response.data[i].images.fixed_height.url);
             showImage.attr("data-state","still");
+            // showImage.attr("class","gif card-deck card-img-top bg-dark col p-0 mt-4 mb-0 text-light border-9  float-right");
             showImage.attr("class","gif");
+
+
+            gifBlock.prepend(showImage);
+            floatDiv.prepend(gifBlock);
+            floatDiv.append('<p id = "note" class="card-title" style="visibility:visible" > Rating : " ' + response.data[i].rating.toUpperCase()+  ' "</p>');
+            // showImage.attr("info", response.data[i].rating.toUpperCase());
             // showImage.attr("data-state", "still");
-            $("#display-area").prepend(showImage);
+            $("#display-area").prepend(floatDiv);
+            
+            // $("#display-area").append('<p id = "note" class="card-title" style="visibility:visible" > Rating : " ' + response.data[i].rating.toUpperCase()+  ' "</p>');
             // console.log(this)
         }
         previousSrch = srchInput;
@@ -62,6 +75,7 @@ $(document).on('click', '.gif', function (){
         $(this).attr("src", $(this).attr("data-animate"));
         console.log("src is " + $(this).attr("src"));
         $(this).attr("data-state","animate"); 
+        // $(this).txt("Rating : "  + $(this).attr("info"));
     } else {
         $(this).attr("src", $(this).attr("data-still"));
         console.log("src is " + $(this).attr("src"));
